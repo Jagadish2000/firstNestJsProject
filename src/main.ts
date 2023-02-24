97cc92d296f66dd9d177c78492f1a8fb17f4181c
Jagadish2000 <71367427+Jagadish2000@users.noreply.github.com>
2023-02-24 11:44:48 +0530

    Add files via upload

---
+++ src/main.ts
@@ -0,0 +1,25 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import {
+  DocumentBuilder,
+  SwaggerDocumentOptions,
+  SwaggerModule,
+} from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Demo API')
+    .setDescription('A Demo API with CRUD functionality')
+    .setVersion('1.0')
+    .build();
+  const options: SwaggerDocumentOptions = {
+    deepScanRoutes: true,
+  };
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(3333);
+}
+bootstrap();
